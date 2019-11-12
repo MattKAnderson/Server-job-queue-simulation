e8c03e2211514b4f03111e33cc7fc9e93e839847
Matt <47264660+MattKAnderson@users.noreply.github.com>
2019-11-12 17:14:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,21 +5,25 @@
 #include "omp.h"
 
 int main(){
-	//multi-threading parameters
 	const int num_threads = 4;
-    //repeats
-    const int num_reps_per_thread = 100;
+    	
+	//repeats
+    	const int num_reps_per_thread = 100;
 	const int total_reps = num_threads * num_reps_per_thread;
+	
 	//simulation parameters
 	const int num_servers = 2;
-    const double a_mean = 6.0;
-    const double p_mean = 10.0;
+    	const double a_mean = 6.0;
+    	const double p_mean = 10.0;
 	const int sim_time = static_cast<const int>(a_mean) * 5000000;
-    //rate parameter is 1 / mean for exponential distribution
+    	
+	//rate parameter is 1 / mean for exponential distribution
 	const double a_rate = 1.0 / a_mean;
 	const double p_rate = 1.0 / p_mean;
+	
 	//results
 	std::vector<int> jobs_queued(total_reps);
+	
 	//simulation objects for part 1 and part 2 respectively
 	simulation part_1(num_servers, a_rate, p_rate, 1);
 	simulation part_2(num_servers, a_rate, p_rate, 2);
